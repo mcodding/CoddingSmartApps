4045c99de209ddca4bbce8b123adb88344ecf1e0
mcodding <31741416+mcodding@users.noreply.github.com>
2018-06-19 14:47:49 -0400

    Update keepmecozyinterlock.groovy

--- smartapps/mcodding/keepmecozyinterlock.src/keepmecozyinterlock.groovy
+++ smartapps/mcodding/keepmecozyinterlock.src/keepmecozyinterlock.groovy
@@ -19,10 +19,10 @@ definition(
     name: "KeepMeCozyInterlock",
     namespace: "mcodding",
     author: "SmartThings",
-    description: "Works the same as Keep Me Cozy, but enables you to pick an alternative temperature sensor in a separate space from the thermostat. Focuses on making you comfortable where you are spending your time rather than where the thermostat is located.",
-    category: "Green Living",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Meta/temp_thermo.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Meta/temp_thermo@2x.png",
+    description: "Enables you to pick an alternative temperature sensor in a separate space to control the thermostat. Also adds an interlock feature to temporarily disable the App",
+    category: "My Apps",
+    iconUrl: "http://icons.iconarchive.com/icons/fatcow/farm-fresh/24/temperature-3-icon.png",
+    iconX2Url: "icons.iconarchive.com/icons/fatcow/farm-fresh/32/temperature-3-icon.png",
     pausable: true
 )
 
